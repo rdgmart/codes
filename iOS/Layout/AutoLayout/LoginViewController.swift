c3a961e87ea37ba729c5de21827364b3be842adb
Rodrigo Martins <contatorodrigo@gmail.com>
2022-08-30 18:08:33 -0400

    Update user interface and delegate login data

--- iOS/Layout/AutoLayout/LoginViewController.swift
+++ iOS/Layout/AutoLayout/LoginViewController.swift
@@ -10,8 +10,28 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    
+    @IBOutlet weak var textLogin: UITextField!
+    
+    @IBOutlet weak var textPwd: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "afterLogin"{
+            
+            if let vcDestination = segue.destination as? ViewController{
+
+                if let text = textLogin.text{
+                    vcDestination.loginUser = text
+                }else {
+                    vcDestination.loginUser = ""
+                }
+                
+            }
+        }
     }
 }
 
