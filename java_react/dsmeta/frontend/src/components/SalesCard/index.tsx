6ecf1746975db0cb4479790e77d309aca7f1e759
Rodrigo Martins <contatorodrigo@gmail.com>
2022-10-04 16:12:57 -0400

    Axios, useEffect first request

--- java_react/dsmeta/frontend/src/components/SalesCard/index.tsx
+++ java_react/dsmeta/frontend/src/components/SalesCard/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import NotificationButton from '../NotificationButton';
@@ -8,6 +9,14 @@ function SalesCard() {
 
     const [minDate, setMinDate] = useState( new Date(new Date().setDate(new Date().getDate() - 60)));
     const [maxDate, setMaxDate] = useState(new Date());
+
+    useEffect(()=>{
+        axios
+            .get("http://localhost:8080/sales")
+            .then(response => {
+                console.log(response.data);
+            });
+    }, []);
 
     return (
         <>
