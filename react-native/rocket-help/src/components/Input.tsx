d0fd5828bd8e358b6037038945d64467b470e1b8
Rodrigo Martins <contatorodrigo@gmail.com>
2022-10-18 17:50:25 -0400

    input component, add dependency phosphoricons

---
+++ react-native/rocket-help/src/components/Input.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Input as InputNativeBase, IInputProps } from 'native-base';
+
+export function Input({... rest}: IInputProps) {
+  return (
+    <InputNativeBase 
+        bg="gray.700"
+        h={14}
+        size="md"
+        borderWidth={0}
+        fontSize="md"
+        fontFamily="body"
+        color="white"
+        placeholderTextColor="gray.300"
+        {... rest}
+    />
+  );
+}
