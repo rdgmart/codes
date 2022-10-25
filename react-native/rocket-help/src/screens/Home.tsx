9f85c1739544790f2e26c9132d19e7a332b12927
Rodrigo Martins <contatorodrigo@gmail.com>
2022-10-25 18:24:21 -0400

    create new component Order and add Flatlist

--- react-native/rocket-help/src/screens/Home.tsx
+++ react-native/rocket-help/src/screens/Home.tsx
@@ -1,13 +1,25 @@
 import { useState } from 'react';
-import { HStack, IconButton, useTheme, VStack, Text, Heading } from 'native-base';
+import { HStack, IconButton, useTheme, VStack, Text, Heading, FlatList, Center } from 'native-base';
+import { ChatTeardrop, ChatTeardropText, SignOut } from 'phosphor-react-native';
 
 import Logo from '../assets/logo_secondary.svg'
-import { SignOut } from 'phosphor-react-native';
+
 import { Filter } from '../components/Filter';
+import { Order, OrderProps } from '../components/Order';
+import { Button } from '../components/Button';
+import { color } from 'native-base/lib/typescript/theme/styled-system';
 
 export function Home() {
     const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('open');
-    
+    const [orders, setOrders] = useState<OrderProps[]>([
+        // {
+        //     id: "123",
+        //     patrimony: "rdgmart-999",
+        //     when: '25/10/2022 16:55',
+        //     status: 'open'
+        // }
+    ])
+
     const { colors } = useTheme();
 
     return (
@@ -43,12 +55,31 @@ export function Home() {
                 </HStack>
 
                 <HStack space={3} mb={8}>
-                    <Filter type='open' title='em andamento' onPress={()=> setStatusSelected('open')} isActive={statusSelected === 'open'} />
+                    <Filter type='open' title='em andamento' onPress={() => setStatusSelected('open')} isActive={statusSelected === 'open'} />
 
-                    <Filter type='closed' title='finalizados' onPress={()=> setStatusSelected('closed')} isActive={statusSelected === 'closed'} />
+                    <Filter type='closed' title='finalizados' onPress={() => setStatusSelected('closed')} isActive={statusSelected === 'closed'} />
 
                 </HStack>
+
+                <FlatList
+                    data={orders}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => <Order data={item} />}
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={{ paddingBottom: 100 }}
+                    ListEmptyComponent={() => (
+                        <Center>
+                            <ChatTeardropText color={colors.gray[300]} size={40}/>
+                            <Text color='gray.300' fontSize='xl' mt={6} textAlign="center">
+                                Você ainda não possui {'\n'}
+                                solicitações {statusSelected === 'open' ? 'em andamento': 'finalizadas'}
+                            </Text>
+                        </Center>
+                    )}
+                />
             </VStack>
+
+            <Button title='Nova Solicitação' />
 
         </VStack>
     );
